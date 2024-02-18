2c4ed53f1f8fc8e740410fdbcff868fd9610b8a3
Shahad <shabduz10@gmail.com>
2024-02-18 17:11:13 +0300

    User Model

---
+++ Sources/App/Models/Users.swift
@@ -0,0 +1,35 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Shahad Alzowaid on 08/08/1445 AH.
+//
+
+import Fluent
+import Vapor
+
+final class Users: Model, Content {
+    static let schema = "user"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "name")
+    var name: String
+    
+    @Field(key: "DoB")
+    var DoB: Date
+    
+    init() { }
+    
+    init(id: UUID? = nil, name: String, DoB : Date) {
+        self.id = id
+        self.name = name
+        self.DoB = DoB
+        
+    
+    }
+ 
+    
+}
+
